cf14e6fed046139aab28352810a983ccc5c63dcd
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-05-12 14:07:12 -0400

    Done.

---
+++ project.js
@@ -0,0 +1,38 @@
+const medicine = [ bleedingControl, cpr, aed, heimlichManeuver ]
+
+function CFR() {
+    const cfrProvider = [oxygen, aspirin, epinephrine-autoInjector, narcan, cervicalCollars, patientAssessment, vitals, bls, operateUnderPhysicianGuidance]
+    console.log(cfrProvider + medicine)
+
+    function EMT() {
+        const emtProvider = [patientTransport, Epinephrine-SyringeNeedle, albuterol, bloodGlucoseMonitoring]
+        console.log(emtProvider + CFR)
+
+        function AdvancedEmt() {
+            const advancedEmtProvider = [ivInsertion, ivFluids, advancedAirways]
+
+            function paramedic() {
+                const paramedicProvider = [intubation, advancedPharmacology, electricalTherapy, cardiacMonitoring]
+            }
+        }
+    }
+}
+
+function cna() {
+    const cnaProvider = [vitals, takeingECG, bloodGlucoseMonitoring]
+
+    function nurse() {
+        const nurseProvider = [ivInsertion, bloodWork, mediationAdministration, triage, patientAssessment]
+
+        function physicianAssistant() {
+            const physicianAssistantProvider = [diagnose, treat, medicationPrescription, intubation]
+
+            function doctor() {
+                const doctorProvider = [practiceMedicineAlone, surgery]
+            }
+        }
+    }
+}
+
+
+CFR()
